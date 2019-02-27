908a7748ea46117d75810b3f651ee48768dfb9d5
vinfry <mercurial.sp@gmail.com>
2019-02-27 17:04:31 +0200

    new plugin

--- src/app.js
+++ src/app.js
@@ -1,63 +1,97 @@
-const exec = require('child_process').exec;
-const q = require('q');
-const async = require('async');
+var exec = require('child_process').exec;
+var Docker = require('dockerode');
+var docker = new Docker();
 
-
-function executeCMD(action) {
-    let deferred = q.defer();
-    let execString = action.method.actionString;
-    for (let i = 0; i < action.method.params.length; i++) {
-        let param = action.method.params[i].name;
-        if (action.params.hasOwnProperty(param)) {
-            execString = execString.replace(param, action.params[param]);
-        }
-        else {
-            execString = execString.replace(param, '');
-        }
-    }
-    exec(execString,
-        function (error, stdout, stderr) {
-            if (error) {
-                return deferred.reject(stderr);
-            }
-            return deferred.resolve(stdout);
-        }
-    );
-    return deferred.promise;
-}
-
-function executeMultiple(action) {
-    let deferred = q.defer();
-
-    let commands = action.params.COMMANDS.split('\n');
-    console.log(commands);
-    let results = [];
-    async.each(commands, function(command, cb) {
-        exec(command,
-            function (error, stdout, stderr) {
-                if (error) {
-                    return cb(error + stderr);
-                }
-                return results.push(stdout);
-            })
-    }, function (error) {
-        if (error) {
-            return deferred.reject(error);
-        }
-        let resString;
-
-        results.forEach((result) => {
-            resString = resString + result + '\n';
+function build(action) {
+    return new Promise((resolve,reject) => {
+        let path = action.params.PATH;
+        let tag = {
+            t: action.params.TAG,
+        };
+        docker.buildImage(path, tag).then(stream=>{
+            docker.modem.followProgress(stream, (err, res) => {
+                if (err) return reject(err);
+                let cmdOutput = "";
+                res.forEach(result=>{
+                    cmdOutput += result.stream;
+                });
+                resolve({output : cmdOutput})
+            });
         });
-        return deferred.resolve()
-    });
-
-    return deferred.promise;
-
+    })
 }
 
+function pull(action) {
+    return new Promise((resolve,reject) => {
+        let image = action.params.IMAGE;
+        let tag = action.params.TAG;
+        docker.pull(image + ":" + tag).then(stream=>{
+            docker.modem.followProgress(stream, (err, res) => {
+            if (err) return reject(err);
+            let cmdOutput = "";
+            res.forEach(result=>{
+                cmdOutput += result.status;
+                });
+            resolve({output: cmdOutput})
+            })
+        })
+    })
+}
+
+function push(action, settings) {
+    return new Promise((resolve, reject) => {
+        let auth = {
+            username: action.params.USER,
+            password: action.params.PASSWORD || settings.PASSWORD,
+        };
+        let reg = action.params.REG;
+        let imageTag = action.params.IMAGETAG;
+        let image = docker.getImage(reg + "/" + imageTag);
+        image.tag({repo: reg + "/" + imageTag}, function () {
+            image.push({authconfig: auth, registry: reg + "/" + imageTag}).then(stream=>{
+                docker.modem.followProgress(stream, (err, res) => {
+                    if (err) return reject(err);
+                    let cmdOutput = "";
+                    res.forEach(result=>{
+                        cmdOutput += result.status + "\r\n";
+                    });
+                    resolve({output: cmdOutput})
+                })
+            })
+        })
+    })
+}
+
+function tag(action) {
+    return new Promise((resolve, reject) => {
+        let sourceReg = action.params.SOURCEIMAGE;
+        let sourceImageTag = action.params.SOURCEIMAGETAG;
+        let newReg = action.params.NEWIMAGE;
+        let newImageTag = action.params.NEWIMAGETAG;
+        let image = docker.getImage(sourceReg + "/" + sourceImageTag);
+        image.tag({repo: newReg + "/" + newImageTag}, function (err, res) {
+            if (err)
+            return reject(err);
+            resolve(res);
+        },)
+    })
+}
+
+function cmdExec(action) {
+    return new Promise((resolve,reject) => {
+        let params = action.params.PARAMS;
+        exec("docker " + params, function (err, stdout, stderr) {
+            if (err || stderr)
+                return reject(err);
+            resolve(stdout)
+        });
+    })
+}
 
 module.exports = {
-    EXECUTE: executeCMD,
-    EXECUTE_MULTIPLE_COMMANDS: executeMultiple
+    build: build,
+    pull: pull,
+    push: push,
+    tag: tag,
+    cmdExec: cmdExec
 };
