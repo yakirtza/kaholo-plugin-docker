eab46af08ec82bf7cf98b5b894dab5a8b48730cb
kadosh1000 <kadosh10001@gmail.com>
2019-03-19 14:50:52 +0200

    * create private methods for common usage * update params labels * update zip * update version to 1.0.0

--- src/app.js
+++ src/app.js
@@ -1,79 +1,70 @@
 var exec = require('child_process').exec;
 var Docker = require('dockerode');
+var nodePath = require('path');
 var docker = new Docker();
 
-function build(action) {
-    return new Promise((resolve,reject) => {
-        let path = action.params.PATH;
-        let tag = {
-            t: action.params.TAG,
-        };
-        docker.buildImage(path, tag).then(stream=>{
-            docker.modem.followProgress(stream, (err, res) => {
-                if (err) return reject(err);
-                let cmdOutput = "";
-                res.forEach(result=>{
-                    cmdOutput += result.stream;
-                });
-                resolve({output : cmdOutput})
+function _getUrl(url, image, tag){
+    return `${url ? `${url}/` : ''}${image}:${tag}`
+}
+
+function _getAuth(action, settings){
+    return {
+        username: action.params.USER || settings.USER,
+        password: action.params.PASSWORD || settings.PASSWORD
+    }
+}
+
+function _streamFollow(stream) {
+    return new Promise((resolve,reject)=>{
+        docker.modem.followProgress(stream, (err, res) => {
+            if (err) return reject(err);
+            let cmdOutput = "";
+            res.forEach(result=>{
+                cmdOutput += result.status;
             });
-        });
+            resolve({output: cmdOutput})
+        })
     })
 }
 
+function build(action) {
+    let path = action.params.PATH;
+    let options = {};
+    if (action.params.TAG)
+        options.t = action.params.TAG;  
+    
+    let parsedPath = nodePath.parse(path);
+
+    return docker.buildImage({
+        context : parsedPath.dir,
+        src : [parsedPath.name]
+    }, options).then(stream=>{
+        return _streamFollow(stream);
+    });
+}
+
 function pull(action, settings) {
-    return new Promise((resolve,reject) => {
-        let auth = {
-            username: action.params.USER,
-            password: settings.PASSWORD
-        };
-        let image = action.params.IMAGE;
-        let tag = action.params.TAG;
-        if (URL) {
-            var options = URL + "/" + image + ":" + tag
-        } else {
-            var options = image + ":" + tag
-        }
-        docker.pull(options, {authconfig: auth}).then(stream=>{
-            docker.modem.followProgress(stream, (err, res) => {
-                if (err) return reject(err);
-                let cmdOutput = "";
-                res.forEach(result=>{
-                    cmdOutput += result.status;
-                });
-                resolve({output: cmdOutput})
-            })
-        })
+    let auth = _getAuth(action,settings);
+    let imageUrl = _getUrl(action.params.URL, action.params.IMAGE, action.params.TAG)
+    
+    return docker.pull(imageUrl, {authconfig: auth}).then(stream=>{
+        return _streamFollow(stream);
     })
 }
 
 function push(action, settings) {
-    return new Promise((resolve, reject) => {
-        let auth = {
-            username: action.params.USER,
-            password: settings.PASSWORD
-        };
-        let imageTag = action.params.IMAGETAG;
-        let imageRepo = action.params.IMAGE;
-        let URL = action.params.URL;
-        let image = docker.getImage(imageRepo + ":" + imageTag);
-        if (URL) {
-            image.tag({repo: URL + "/" + imageRepo + ":" + imageTag});
-            var imageToPush = docker.getImage(URL + "/" + imageRepo + ":" + imageTag);
-        } else {
-            image.tag({repo: imageRepo + ":" + imageTag});
-            var imageToPush = docker.getImage(imageRepo + ":" + imageTag);
-        }
-        imageToPush.push({authconfig: auth, registry: imageRepo + ":" + imageTag}).then(stream=>{
-            docker.modem.followProgress(stream, (err, res) => {
-                if (err) return reject(err);
-                let cmdOutput = "";
-                res.forEach(result=>{
-                    cmdOutput += result.status + "\r\n";
-                });
-                resolve({output: cmdOutput})
-            })
-        })
+    let imageTag = action.params.IMAGETAG;
+    let imageRepo = action.params.IMAGE;
+    
+    let auth = _getAuth(action,settings);
+    let imageUrl = _getUrl(action.params.URL, imageRepo, imageTag);
+    let image = docker.getImage(imageRepo + ":" + imageTag);
+
+    image.tag({repo: imageUrl});
+    var imageToPush = docker.getImage(imageUrl);
+    
+    return imageToPush.push({authconfig: auth, registry: imageUrl}).then(stream=>{
+        return _streamFollow(stream);
     })
 }
 
@@ -87,9 +78,9 @@ function tag(action) {
         let image = docker.getImage(sourceReg + "/" + sourceImageTag);
         image.tag({repo: newReg + "/" + newImageTag}, function (err, res) {
             if (err)
-            return reject(err);
+                return reject(err);
             resolve(res);
-        },)
+        })
     })
 }
 
@@ -98,7 +89,7 @@ function cmdExec(action) {
         let params = action.params.PARAMS;
         exec("docker " + params, function (err, stdout, stderr) {
             if (err || stderr)
-                return reject(err);
+                return reject(err || stderr);
             resolve(stdout)
         });
     })
